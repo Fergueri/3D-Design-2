15567e03d342fb849b855a9dac0bfb4f84d57b83
Eric Ferguson <68354094+Fergueri@users.noreply.github.com>
2021-02-17 22:32:17 -0500

    Update light_directions_withbutton.ino

--- light_directions_withbutton.ino
+++ light_directions_withbutton.ino
@@ -2,7 +2,10 @@
 #include <Adafruit_NeoPixel.h>
 #define PIN 2
 #define NUMPIXELS 4
-
+const int buttonPin1 = 5;
+const int buttonPin2 = 6;
+int buttonState1 = 0
+int buttonState2 = 0
 Adafruit_NeoPixel strip(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
@@ -10,27 +13,32 @@ void setup() {
   strip.begin();
   strip.setBrightness(50);
   strip.show();
-  
-  Serial.begin(9600);
+  pinMode(buttonPin1, INPUT);
+  pinMode(buttonPin2, INPUT);
 }
 
 void loop() {
-  int sensorValue = analogRead(A0);
-  Serial.println(sensorValue);
-  delay(1);
+  buttonState = digitalRead(buttonPin);
+  if(buttonState1 == 1){
+    right();  
+  }
+ if(buttonState1 == 1){
+    right();  
+  }
+
 }
-  void right() {
-    colorWipe1(strip.Color(255, 0, 0),50); // Red
-    //colorWipe1(strip.Color(0, 255, 0), 50); // Green
-   // colorWipe1(strip.Color(0, 0, 255), 50); // Blue
-      
-  }
-  void left() {
-    //colorWipe2(strip.Color(255, 0, 0),50); // Red
-      colorWipe2(strip.Color(0, 255, 0), 50); // Green
-   // colorWipe2(strip.Color(0, 0, 255), 50); // Blue
-      
-  }
+void right() {
+  colorWipe1(strip.Color(255, 0, 0),50); // Red
+  colorWipe1(strip.Color(0, 255, 0), 50); // Green
+  colorWipe1(strip.Color(0, 0, 255), 50); // Blue
+    
+}
+void left() {
+  colorWipe2(strip.Color(255, 0, 0),50); // Red
+  colorWipe2(strip.Color(0, 255, 0), 50); // Green
+  colorWipe2(strip.Color(0, 0, 255), 50); // Blue
+    
+}
 
 
 void colorWipe2(uint32_t c, uint8_t wait){
@@ -41,7 +49,7 @@ void colorWipe2(uint32_t c, uint8_t wait){
   }  
 }
 void colorWipe1(uint32_t c, uint8_t wait){
-  for(uint32_t i=0; i<strip.numPixels(); i++) {
+  for(uint32_t i=0; i<strip.numPixels; i++) {
     strip.setPixelColor(i, c);
     strip.show();
     delay(wait);
