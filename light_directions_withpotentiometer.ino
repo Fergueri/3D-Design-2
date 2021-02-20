80545af7e96e843f177a113a10a14e1a56f3bf81
Eric Ferguson <68354094+Fergueri@users.noreply.github.com>
2021-02-20 01:44:22 -0500

    Update light_directions_withpotentiometer.ino

--- light_directions_withpotentiometer.ino
+++ light_directions_withpotentiometer.ino
@@ -1,29 +1,29 @@
 
 #include <Adafruit_NeoPixel.h>
-#define PIN 2
-#define NUMPIXELS 4
+#define PIN 2 //what pin the neopixel is in
+#define NUMPIXELS 4 // how many neopixels there are
 
 Adafruit_NeoPixel strip(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   // put your setup code here, to run once:
-  strip.begin();
-  strip.setBrightness(50);
+  strip.begin();//starts the neopixels
+  strip.setBrightness(50);// sets the brightness of the neopixel
   strip.show();
   
   Serial.begin(9600);
 }
 
-void loop() {
+void loop() {// reads the serial numbner of the potentiometer at anytime to adjust
   int sensorValue = analogRead(A0);
-  if(sensorValue <= 512){
+  if(sensorValue <= 512){//if the serial is below or equal to 512 the colorWipe goes right
       right();
     } 
-  else if(sensorValue > 512){
+  else if(sensorValue > 512){//else the color wipe goes right
       left();
     }
 }
-  void right() {
+  void right() { //initalizes the colorWipes and sets their colors
     colorWipe1(strip.Color(255, 0, 0),50); // Red
     colorWipe1(strip.Color(0, 255, 0), 50); // Green
     colorWipe1(strip.Color(0, 0, 255), 50); // Blue
@@ -37,14 +37,14 @@ void loop() {
   }
 
 
-void colorWipe2(uint32_t c, uint8_t wait){
+void colorWipe2(uint32_t c, uint8_t wait){ //loop for the color wipe the goes right
   for(uint32_t i=4; i<=4; i--) {
     strip.setPixelColor(i, c);
     strip.show();
     delay(wait);
   }  
 }
-void colorWipe1(uint32_t c, uint8_t wait){
+void colorWipe1(uint32_t c, uint8_t wait){//loop for the color wipe the goes left
   for(uint32_t i=0; i<strip.numPixels(); i++) {
     strip.setPixelColor(i, c);
     strip.show();
