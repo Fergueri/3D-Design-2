265859327dc695c2dbb75bd929c729e70b88ce7a
Eric Ferguson <68354094+Fergueri@users.noreply.github.com>
2021-02-20 00:52:44 -0500

    Update basic_neopixel_code.ino

--- basic_neopixel_code.ino
+++ basic_neopixel_code.ino
@@ -1,18 +1,18 @@
 
 #include <Adafruit_NeoPixel.h>
-#define PIN 2
-#define NUMPIXEL 4
+#define PIN 2 //pin the neopixels are in
+#define NUMPIXEL 4 // how many neopixels there are
 
 Adafruit_NeoPixel strip(NUMPIXEL, PIN);
 
 void setup() {
   // put your setup code here, to run once:
-  strip.begin();
+  strip.begin(); //starts the neopixels
   strip.setBrightness(20);
   
 }
 
-void loop() {
+void loop() {//loops the colors
   colorWipe(strip.Color(255, 0, 0),50); // Red
   colorWipe(strip.Color(0, 255, 0), 50); // Green
   colorWipe(strip.Color(0, 0, 255), 50); // Blue
@@ -23,9 +23,9 @@ void loop() {
 }
 
 void colorWipe(uint32_t c, uint8_t wait){
-  for(uint32_t i=0; i<strip.numPixels(); i++) {
-    strip.setPixelColor(i, c);
-    strip.show();
+  for(uint32_t i=0; i<strip.numPixels(); i++) { //loops the colorWipe
+    strip.setPixelColor(i, c);//sets the colors. i is the pixel number and c is the rgb code
+    strip.show(); //shows the neopixels
     delay(250);
   }  
 }
